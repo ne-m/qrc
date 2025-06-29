0cb9ab4c34b778059de91819bc5f1a5317eb7897
ne-m <11538emmanuelmurithi@gmail.com>
2025-06-29 22:40:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
+let downloadBtn = document.querySelector("a:);
 
 function generateQR() {
     if (qrText.value.length > 0) {
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-        imgBox.classList.add("show-img");        
+        imgBox.classList.add("show-img");   
+        downloadBtn.href = qrImage.src;
+        downloadBtn.classList.add("showDownloadBtn");
     }
 }
 
